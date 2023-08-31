12d07e1288e1fb221c6d2e864dfe08f8863c3728
Brandon <name.brandon.aguero@gmail.com>
2023-08-31 15:07:37 -0500

    Ahora es responsive para dispositivos moviles, tablets y desktops

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import QuestionsList from "./components/QuestionsList";
 function App(): JSX.Element {
   return (
     <>
-      <main className="am:gap-4 relative grid h-[80%] max-h-[34rem] w-[90%] grid-cols-6 grid-rows-6 gap-2 rounded-lg bg-slate-50 py-4">
-        <div className="absolute"></div>
-        <h1 className="col-start-1 col-end-7 row-start-1 row-end-2 px-4 text-center text-2xl text-indigo-950">
+      <main className="am:gap-4 relative z-10 grid h-[80%] max-h-[34rem] w-[90%] max-w-[53.75rem] grid-cols-6 grid-rows-6 gap-2 rounded-lg bg-slate-50 py-4 sm:py-12">
+        <h1 className="col-start-1 col-end-7 row-start-1 row-end-2 px-4 text-center text-2xl text-indigo-950 sm:col-end-3 sm:row-end-7 md:text-3xl">
           Questions and answers about login
         </h1>
         <QuestionsList />
-        <div className="absolute"></div>
       </main>
     </>
   );
--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -12,7 +12,7 @@ const QuestionsList: React.FC = () => {
   const questions: QuestionData[] = questionsData;
 
   return (
-    <aside className="col-start-1 col-end-7 row-start-2 row-end-7 flex h-full flex-col justify-between gap-2 overflow-y-auto overscroll-y-auto px-4">
+    <aside className="col-start-1 col-end-7 row-start-2 row-end-7 flex h-full flex-col justify-between gap-2 overflow-y-auto overscroll-y-auto px-4 sm:col-start-3 sm:row-start-1">
       {questions.map((question) => (
         <Question
           key={question.id}
